304bc0a6ef38e400f664938ed4e75477e11691fd
Isaac Gifford <isaac@geekingreen.com>
2018-10-21 18:54:36 -0600

    add comments

--- main.go
+++ main.go
@@ -7,38 +7,41 @@ import "github.com/googollee/go-socket.io"
 
 // Message json object
 type Message struct {
-	Username string `json:"username"`
-	Message  string `json:"message"`
+  Username string `json:"username"`
+  Message  string `json:"message"`
 }
 
 func main() {
-	server, err := socketio.NewServer(nil)
+  server, err := socketio.NewServer(nil)
 
-	if err != nil {
-		log.Fatal(err)
-	}
+  if err != nil {
+    log.Fatal(err)
+  }
 
-	server.On("connection", func(socket socketio.Socket) {
-		log.Println("socket " + socket.Id() + " connected")
+  server.On("connection", func(socket socketio.Socket) {
+    log.Println("socket " + socket.Id() + " connected")
 
-		socket.Join("chat")
+    socket.Join("chat")
 
-		socket.On("chatMessage", func(msg Message) {
-			log.Println("emit:", socket.Emit("chatMessage", msg))
-			socket.BroadcastTo("chat", "chatMessage", msg)
-		})
+    socket.On("chatMessage", func(msg Message) {
+      log.Println("emit: " + msg.Message)
+      // Send to self
+      socket.Emit("chatMessage", msg)
+      // Send to everyone else
+      socket.BroadcastTo("chat", "chatMessage", msg)
+    })
 
-		socket.On("disconnect", func() {
-			log.Println("on disconnect")
-		})
-	})
+    socket.On("disconnect", func() {
+      log.Println("on disconnect")
+    })
+  })
 
-	server.On("error", func(socket socketio.Socket, err error) {
-		log.Println("error:", err)
-	})
+  server.On("error", func(socket socketio.Socket, err error) {
+    log.Println("error:", err)
+  })
 
-	http.Handle("/socket.io/", server)
-	http.Handle("/", http.FileServer(http.Dir("./public")))
-	log.Println("server running on localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+  http.Handle("/socket.io/", server)
+  http.Handle("/", http.FileServer(http.Dir("./public")))
+  log.Println("server running on localhost:8080")
+  log.Fatal(http.ListenAndServe(":8080", nil))
 }
